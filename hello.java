73411884a87bf40ef1b89f6c5e7425e43b0cba75
computer system engineering <cse@localhost.localdomain>
2018-10-28 14:25:22 +0900

    Commit 4

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ public static void main(String [] args){
   System.out.println("Hello github");
   System.out.println("For commit 2");
   System.out.println("For commit 3");
+  System.out.println("For commit 4");
 }
 }
