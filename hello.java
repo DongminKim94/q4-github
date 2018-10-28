5cd39c26d8daefff4a999417df01aeb29bffb379
Eastsky94 <dongals94@daum.net>
2018-10-28 14:18:22 +0900

    Commit 3

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
 public static void main(String [] args){
   System.out.println("Hello github");
   System.out.println("For commit 2");
+  System.out.println("For commit 3");
 }
 }
